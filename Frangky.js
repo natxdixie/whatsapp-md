694ad0aedf90f7e2ed982749808d832c551cecde
Frangky <81846311+Franky404@users.noreply.github.com>
2021-12-15 23:59:57 +0700

    Update Frangky.js

--- Frangky.js
+++ Frangky.js
@@ -663,21 +663,22 @@ ${ttt}`
             
             
             
-                if (budy.startsWith('=>')) {
-                    if (!isCreator) return m.reply(mess.owner)
-                    function Return(sul) {
-                        sat = JSON.stringify(sul, null, 2)
-                        bang = util.format(sat)
-                            if (sat == undefined) {
-                                bang = util.format(sul)
-                            }
-                            return m.reply(bang)
-                    }
-                    try {
-                        m.reply(util.format(eval(`(async () => { return ${budy.slice(3)} })()`)))
-                    } catch (e) {
-                        m.reply(String(e))
-                    }
+                if (budy.startsWith('=>')){
+                if (!isCreator) return
+                var konsol = budy.slice(2)
+                Return = (sul) => {
+                var sat = JSON.stringify(sul, null, 2)
+                bang = util.format(sat)
+                if (sat == undefined){
+                bang = util.format(sul)
+                }
+                return m.reply(bang)
+                }
+                try {
+                m.reply(util.format(eval(`;(async () => { ${konsol} })()`)))
+                } catch(e){
+                 m.reply(String(e))
+                }
                 }
 
                 if (budy.startsWith('>')) {
